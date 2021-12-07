8e92af419efbd3e6d255359dd447f0d1765b1b53
Dennisbullys <d.rochus@hotmail.de>
2021-12-07 13:21:10 +0100

    Dennis

---
+++ Dennis.js
@@ -0,0 +1,20 @@
+// javascript-recap-functions-with-params
+
+
+const gastliste = ["Chuck","John","Peter","Robert","Diana","Lucy","Bob","Charls","Anabelle","Ben","Simon"]
+
+
+
+
+
+console.log(
+  gastliste.includes(
+    "Chuck")
+)
+   
+
+
+
+
+
+
