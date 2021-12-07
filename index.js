a8cd77e11629c3d88748f867cf6db2d3e3448a6e
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-12-07 13:02:31 +0100

    js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const gastliste = ["Chuck", "John", "Peter", "Robert", "Diana", "Lucy", "Bob", "Charls", "Anabelle", "Ben" , "Simon"];
 console.log(gastliste);
 function istEnthalten (gast) {
-    let anwesend = gastliste.includes(gast);
-    console.log(anwesend);
+    let Werten = gastliste.includes(gast);
+    console.log(Werten);
 }
 istEnthalten("Sarah");
 
 function istEnthalten (gast) {
-    let anwesend = gastliste.includes(gast);
-    console.log(anwesend);
+    let Werten = gastliste.includes(gast);
+    console.log(Werten);
 }
 istEnthalten("John");
