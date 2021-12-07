5d58f9bbb86baadadee4541f6676e8e7165c697c
Mazen <mazen.taha88@gmail.com>
2021-12-08 00:42:17 +0100

    My Lösung

---
+++ index.js
@@ -0,0 +1,18 @@
+let gastliste = ["Chuck", "John", "Peter", "Robert", "Diana"];
+
+
+
+
+function gäste(name) {
+
+     if( gastliste.includes(name)) {
+         console.log(true);
+     }else {
+         console.log(false);
+     }
+
+ }
+ gäste("Oliv");
+ gäste("John");
+
+ 
