578033782f480979960822fbe093997eaf37b3c6
ShiinyFlakes <mago_elm@hotmail.com>
2021-12-07 13:15:31 +0100

    Lösung

---
+++ script.js
@@ -0,0 +1,4 @@
+const gaesteliste = ["Cuck ", "John ", "Peter ", "Robert ", "Diana ","Lucy", "Bob", "Charles", "Anabekke", "ben", "Simon" ];
+gaesteliste.includes("Mago", 2);
+console.log(gaesteliste.includes("Mago"));
+
