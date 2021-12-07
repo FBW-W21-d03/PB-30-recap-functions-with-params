fceb23a46534ce579a66c90a8f08223ecae744bc
sprotte <metatron1972@live.de>
2021-12-07 12:45:24 +0100

    sprotte

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 
-let objekt = ["Chuck", "John", "Peter", "Robert", "Diana", "Lucy", "Bob", "Charls", "Anabelle", "Ben", "Simon"];  // Array
-console.log(objekt);
+const gastebuch = ["Chuck", "John", "Peter", "Robert", "Diana", "Lucy", "Bob", "Charls", "Anabelle", "Ben", "Simon"];  // Array
+
+
+function gasteliste(Gast) {
+
+    if (gastebuch.indexOf(Gast) !== -2) {
+        console.log("Bob",true);
+    } else {
+        console.log("Metatron",false);
+    }
+    
+}
+
