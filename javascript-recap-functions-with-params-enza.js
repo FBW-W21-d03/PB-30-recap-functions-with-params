c132d5e852a224724c51e79bbd18f4ae475065dd
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-12-07 12:27:48 +0100

    mühsam ernährt sich das eichhörnchen..

---
+++ javascript-recap-functions-with-params-enza.js
@@ -0,0 +1,15 @@
+const gastListe = ["Chuck", "John", "Peter", "Robert", "Diana", "Lucy", "Bob", "Charls", "Anabelle", "Ben" , "Simon"];
+
+function istEinGast (gast) {
+    let anwesend = gastListe.includes(gast);
+    console.log(anwesend);
+}
+istEinGast("enza");
+
+function istEinGast (gast) {
+    let anwesend = gastListe.includes(gast);
+    console.log(anwesend);
+}
+istEinGast("Bob");
+
+//puh!
