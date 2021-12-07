68f117e8016d1d5b1a51e3f34d728a9f3d18c4a9
kawanaza <kawanbabani@gmail.com>
2021-12-07 13:42:52 +0100

    Done

--- main.js
+++ main.js
@@ -2,4 +2,11 @@
 
 const Gästeliste = ["Chuck", "John", "Peter", "Robert", "Diana", "Lucy", "Bob", "Charls", "Anabelle", "Ben" , "Simon"];
 Gästeliste.includes("kawan",2)
-console.log(Gästeliste.includes("kawan"))
+console.log(Gästeliste.includes("kawan"));
+
+
+
+document.getElementById("gast").innerHTML = Gästeliste.includes("kawan",2);
+
+
+
